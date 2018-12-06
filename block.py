6cc0055054e606564b20b22108ad5cfb1b8ee544
imaliazhar <aliazharthegreat@live.com>
2018-12-07 00:10:46 +0500

    added nonce

--- block.py
+++ block.py
@@ -6,9 +6,18 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
+        self.nonce = 0
         self.hash = self.hash_block()
 
     def hash_block(self):
         sha = hashlib.sha256()
-        sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode('utf-8'))
+        sha.update((str(self.index) + str(self.timestamp) + str(self.data) 
+            + str(self.previous_hash) + str(self.nonce)).encode('utf-8'))
         return sha.hexdigest()
+    
+    def mine_block(self, difficulty):
+        while self.hash[0, difficulty] != '0' * difficulty:
+          self.nonce = self.nonce + 1
+          self.hash = self.hash_block()
+      
+        print("Block mined: " + self.hash)
