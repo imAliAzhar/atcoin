88147d38cd53d185ce6a904c9f620ef3c41600ac
imaliazhar <aliazharthegreat@live.com>
2018-12-09 01:40:25 +0500

    added buy and get_balance functions

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import jsonpickle
 
@@ -5,17 +6,38 @@ from wallet import Wallet
 from transaction import Transaction
 
 
+URL = 'http://localhost:3000/'
+
 class Client():
+    def __init__(self, name):
+        self.wallet = Wallet(name)
+
     def send_transaction(self):
         NotImplemented
     
-    def send(self):
-        url = 'http://localhost:5000/get_blocks'
-        t = Transaction("Ali", "Umar", 500)
-        headers = {"Content-Type": "application/json"}
-        payload = jsonpickle.encode(t)
-        res = requests.post(url, data=payload, headers=headers)
-        print(res.text)
+    def buy(self, amount):
+        tx = Transaction(self.wallet.public_key, self.wallet.public_key, amount, "buy")
+        self.send(tx)
 
-client = Client()
-client.send()
+    def get_balance(self):
+        url = URL + 'balance'
+        payload = {'user_id': self.wallet.public_key}
+        response = requests.get(url, params=payload)
+        print("Current Balance:", response.text)
+
+    def send(self, data):
+        url = URL + 'transactions'
+        headers = {"Content-Type": "application/json"}
+        payload = jsonpickle.encode(data)
+        response = requests.post(url, data=payload, headers=headers)
+        print(response.text)
+
+###############################################################
+
+if len(sys.argv) < 2:
+    print("Please pass client's name as parameter")
+    exit()
+
+client = Client(sys.argv[1])
+client.buy(50)
+client.get_balance()
