7988f4744fbc9f9c4d4fb22767dda8e4b3f349bb
imaliazhar <aliazharthegreat@live.com>
2018-12-09 01:42:25 +0500

    added kinds for transactions

--- transaction.py
+++ transaction.py
@@ -4,24 +4,28 @@ import json
 from record import Record
 
 class Transaction:
-    def __init__(self, frm, to, amount):
-        self.frm = None
-        self.to = None
-        self.amount = None
+    def __init__(self, frm, to, amount, kind="regular"):
+        self.frm = frm
+        self.to = to
+        self.amount = amount
+        self.kind = kind
         self.input = []
         self.output = []
 
     def commit(self, chain):
-        self.input = chain.get_unspent_records()
-        unspent_amount = 0
-        for record in self.input:
-            unspent_amount = unspent_amount + record.amount
-        if self.amount > unspent_amount:
-            return "Insufficient balance for this transaction. Current balance: " + str(unspent_amount)
-        remainder_amount = unspent_amount - self.amount
-        self.output.append(Record(self.to, self.amount))
-        self.output.append(Record(self.frm, remainder_amount))
-        return True
+        if self.kind == "regular":
+            self.input = chain.get_unspent_records(self.frm)
+            unspent_amount = chain.get_balance(self.frm)
+            if self.amount > unspent_amount:
+                return "Insufficient balance for this transaction. Current balance: " + str(unspent_amount)
+            remainder_amount = unspent_amount - self.amount
+            self.output.append(Record(self.to, self.amount))
+            self.output.append(Record(self.frm, remainder_amount))
+            return True
+        elif self.kind == "buy":
+            self.output.append(Record(self.to, self.amount))
+            return True
+
 
         
 
